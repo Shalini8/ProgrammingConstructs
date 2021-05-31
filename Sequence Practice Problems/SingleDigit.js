ed7459490def58dc990cc3fe54de2605b7651246
Shalini8 <shalu8mar@gmail.com>
2021-05-31 08:17:02 +0530

    all the programming constructs que solved

---
+++ Sequence Practice Problems/SingleDigit.js
@@ -0,0 +1,6 @@
+a = Math.floor(Math.random() * 10);
+console.log(a);
+b = Math.floor(Math.random()*6);
+console.log(b);
+c = a+b;
+console.log("addition is " +c);
---
+++ Sequence Practice Problems/SumAvg.js
@@ -0,0 +1,9 @@
+let a=Math.floor(Math.random() * 100);
+let b=Math.floor(Math.random() * 100);
+let c=Math.floor(Math.random() * 100);
+let d=Math.floor(Math.random() * 100);
+let e=Math.floor(Math.random() * 100);
+ sum = (a+b+c+d+e);
+avg = ((a+b+c+d+e)/5);
+console.log("sum is" +sum);
+console.log("Average is" +avg);
